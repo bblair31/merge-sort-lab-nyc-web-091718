c1507d72c9bd8ce11fe192562cfe4b0773420603
Brennan Blair <bblair31@users.noreply.github.com>
2019-01-19 12:48:17 -0500

    complete findMin and merge

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function findMinAndRemoveSorted(array) {
+  return array.shift()
+}
 
+function merge(array1, array2) {
+  let mergedArray = []
+
+  while (array1.length !== 0 && array2.length !== 0) {
+    if (array1[0] < array2[0]) {
+      mergedArray.push(findMinAndRemoveSorted(array1))
+    } else {
+      mergedArray.push(findMinAndRemoveSorted(array2))
+    }
+  }
+  return mergedArray.concat(array1).concat(array2)
+}
